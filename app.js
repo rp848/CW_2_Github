5848cfa7cb612f04866a17ff309f6f34ef792ce6
RP848_MDX <rp848@live.mdx.ac.uk>
2020-02-10 22:46:57 +0000

    update

---
+++ app.js
@@ -0,0 +1,54 @@
+if ('serviceWorker'in navigator)
+{navigator.serviceWorker.register('/coursework2/sw.js');
+};
+
+
+ 
+var button = document.getElementById("notifications");
+button.addEventListener('click', function(e) {
+Notification.requestPermission().then(function(result) { 
+if(result === 'granted') { 
+randomNotification(); 
+}
+});
+});
+
+
+function randomNotification(){
+    var randomItem = Math.floor(Math.random()*courses.length);
+    var notifTitle = courses[randomItem].topic;
+    var notifBody='Created by '+courses[randomItem].provider+'.';
+    var notifImg='data/images/'+courses[randomItem].slug+'.jpg';
+    var options = {body:notifBody,
+        icon:notifImg
+    }
+    var notif = new Notification(notifTitle,options);
+    setTimeout(randomNotification,300000000);
+}
+
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+const loadImages = (image) => {
+    image.setAttribute('src' , image.getAttribute('data-src'));
+    image.onload =() => {
+        image.removeAttribute('data-src');
+    };
+};
+
+if('IntersectionObserver' in window) {
+	var observer = new IntersectionObserver(function(items, observer) {
+		items.forEach(function(item) {
+			if(item.isIntersecting) {
+				loadImages(item.target);
+				observer.unobserve(item.target);
+			}
+		});
+	});
+	imagesToLoad.forEach(function(img) {
+		observer.observe(img);
+	});
+}
+else {
+	imagesToLoad.forEach(function(img) {
+		loadImages(img);
+	});
+}
---
+++ sw.js
@@ -0,0 +1,74 @@
+var cacheName = 'cacheFiles-v1'; 
+
+var appShellFiles = [
+    '/coursework2/',
+    '/coursework2/data/images/business.png',
+    '/coursework2/data/images/CS.png',
+    '/coursework2/data/images/dance.png',
+    '/coursework2/data/images/drama.png',
+    '/coursework2/data/images/english.png',
+    '/coursework2/data/images/maths.png',
+    '/coursework2/data/images/music.png',
+    '/coursework2/data/images/PE.png',
+    '/coursework2/data/images/pshe.png',
+    '/coursework2/data/images/science.png',
+    '/coursework2/index.html',
+    '/coursework2/server.js',
+    '/coursework2/sw.js',
+    '/coursework2/app.js',
+
+];
+
+var contentToCache = [] = appShellFiles;
+
+self.addEventListener('install',(e)=>{
+    console.log('[Service Worker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then((cache) => {
+             console.log('[Service Worker] Caching all: app shell and content');
+             return cache.addAll(contentToCache);
+            })
+            );
+        }
+ );
+
+ self.addEventListener('fetch', function (e) {
+     e.respondWith(
+         caches.match(e.request).then(function(r){
+             console.log('[Service Worker] Fetching resource: '+e.request.url);
+             return r || fetch(e.request).then( function (response) {
+                 return caches.open(cacheName).then(function (cache)
+                 {
+                console.log('[Service Worker] Caching new resource: '+e.request.url);
+                cache.put(e.request, response.clone());
+                return response;
+            });
+        });
+    }));
+});
+
+self.addEventListener('install',(e) => {
+    e.waitUntil(
+    caches.open('cacheFiles-v1').then((cache) => {
+        return cache.addAll(contentToCache);
+    }));
+});
+
+self.addEventListener('activate',(e) => {
+
+    e.waitUntil(
+    caches.keys().then((keyList) => {
+    return Promise.all(keyList.map((key) => { 
+    if (key !== cacheName) { 
+    return caches.delete(key);
+    }}
+    ));
+    }));
+    });
+
+
+  
+
+
+
+
